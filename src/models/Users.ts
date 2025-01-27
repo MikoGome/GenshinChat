3b33a8f2a4c6f5a7d21919a27ebbcad59cc2bbd6
Miko <89363492+MikoGome@users.noreply.github.com>
2025-01-27 04:13:18 -0800

    migrate to new sql database

--- src/models/Users.ts
+++ src/models/Users.ts
@@ -2,9 +2,25 @@ import pg from 'pg';
 import dotenv from 'dotenv'
 dotenv.config();
 
+const config = {
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync("./ca.pem").toString(),
+  },
+};
+
+/*
 const pool = new pg.Pool({
   connectionString: process.env.PG_URI
 });
+*/
+
+const pool = new pg.Client(config);
 
 pool.connect()
   .then(():void => {
